19d833c53de7a1b21443137eee35c6d525d7235f
agilc <agilc315@gmail.com>
2020-03-30 23:39:14 +0530

    added auth header

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "*");
   next();
 });
